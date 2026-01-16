7e4b59c1545bc9b48fee28363de16a50afffa70d
Pavel Shchichko <pavel.schko@devsisters.com>
2026-01-16 18:36:35 +0900

    Use bit array to keep tags

--- core/shared/src/main/scala/zio/prelude/fx/TagStack.scala
+++ core/shared/src/main/scala/zio/prelude/fx/TagStack.scala
@@ -6,9 +6,10 @@ package zio.prelude.fx
 private final class TagStack[A <: AnyRef] { self =>
   import TagStack._
 
-  private[this] var array  = new Array[AnyRef](ArrSize + 1)
-  private[this] var packed = 0
-  array(ArrSize) = new Array[Boolean](ArrSize)
+  private[this] var array      = new Array[AnyRef](ArrSize + 1)
+  private[this] var packed     = 0
+  private[this] var packedTags = 0
+  array(ArrSize) = new Array[Int](1)
 
   def clear(): Unit = {
     var i = 0
@@ -28,16 +29,22 @@ private final class TagStack[A <: AnyRef] { self =>
     val array   = this.array
     if (used == ArrSize) {
       val newArr    = new Array[AnyRef](ArrSize + 1)
-      val newTagArr = new Array[Boolean](ArrSize)
+      val newTagArr = new Array[Int](1)
       newArr(ArrSize) = newTagArr
       newArr(0) = array
       newArr(1) = a
-      newTagArr(1) = tag
+      packedTags = if (tag) 2 else 0
+      newTagArr(0) = packedTags
       this.array = newArr
       packed += 3
     } else {
       array(used) = a
-      (array(ArrSize).asInstanceOf[Array[Boolean]])(used) = tag
+      if (tag) {
+        packedTags |= 1 << used
+      } else {
+        packedTags &= ~(1 << used)
+      }
+      (array(ArrSize).asInstanceOf[Array[Int]])(0) = packedTags
       packed += 1
     }
   }
@@ -56,6 +63,7 @@ private final class TagStack[A <: AnyRef] { self =>
       if (idx == 0 && packed0 != 1) {
         val arr0 = a.asInstanceOf[Array[AnyRef]]
         a = arr0(ArrSize - 1)
+        packedTags = arr0(ArrSize).asInstanceOf[Array[Int]](0)
         array = arr0
         packed -= 3
       } else {
@@ -73,10 +81,10 @@ private final class TagStack[A <: AnyRef] { self =>
       val used = packed0 & 0xf
       val idx  = used - 1
       if (idx == 0 && packed0 != 1) {
-        val tagArray = (array(idx).asInstanceOf[Array[AnyRef]])(ArrSize).asInstanceOf[Array[Boolean]]
-        tagArray(ArrSize - 1)
+        val tags = (array(idx).asInstanceOf[Array[AnyRef]])(ArrSize).asInstanceOf[Array[Int]](0)
+        (tags >> (ArrSize - 1) & 1) == 1
       } else {
-        (array(ArrSize).asInstanceOf[Array[Boolean]])(idx)
+        (packedTags >> idx & 1) == 1
       }
     }
   }
